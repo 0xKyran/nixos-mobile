dcbbe02f7b26788a48d3f8ebbbbd48a61d25eed0
0xKyran <kyran@baarda.xyz>
2023-09-24 01:28:02 +0200

    default.nix

---
+++ default.nix
@@ -0,0 +1,44 @@
+{ device ? null
+, configuration ? null
+, pkgs ? (import ./pkgs.nix {})
+}@args':
+
+let
+  # Inherit default values correctly in `args`
+  args = args' // {
+    inherit pkgs;
+  };
+
+  defaultConfiguration =
+    if configuration != null then
+      configuration
+    else if (builtins.pathExists ./local.nix) then
+      builtins.trace ''
+        ${"\n"}
+        ********************************************
+        * WARNING: evaluation includes ./local.nix *
+        ********************************************
+      '' ./local.nix
+    else
+      {}
+  ;
+in
+
+import ./lib/eval-with-configuration.nix (args // {
+  configuration = [
+    defaultConfiguration
+  ];
+  additionalHelpInstructions = ''
+    You can build the `-A outputs.default` attribute to build an empty and
+    un-configured image. That image can be configured using `local.nix`.
+
+     ** Note that an unconfigured image may appear to hang at boot. **
+
+    An alternative is to use one of the `examples` system. They differ in their
+    configuration. An example that should be building, and working using
+    cross-compilation is the `examples/hello` system. Read its README for more
+    information.
+
+     $ nix-build examples/hello --argstr device ${device} -A outputs.default
+  '';
+})
